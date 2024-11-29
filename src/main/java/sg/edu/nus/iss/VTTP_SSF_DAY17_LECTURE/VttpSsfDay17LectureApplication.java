9a96d1abd6852a696db7825a3323ffe9b3f205dd
Lawson Kwek <63045323+lkas96@users.noreply.github.com>
2024-11-29 09:20:15 +0800

    Springboot Initializer

---
+++ src/main/java/sg/edu/nus/iss/VTTP_SSF_DAY17_LECTURE/VttpSsfDay17LectureApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.VTTP_SSF_DAY17_LECTURE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VttpSsfDay17LectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VttpSsfDay17LectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/VTTP_SSF_DAY17_LECTURE/VttpSsfDay17LectureApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.VTTP_SSF_DAY17_LECTURE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VttpSsfDay17LectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
